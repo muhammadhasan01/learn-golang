48a4de2ab8445ef8952c54b7d68a0dc016a6b758
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-09 17:05:35 +0700

    jwt for the win

--- gorm/main.go
+++ gorm/main.go
@@ -1,27 +1,116 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func main() {
-	db, err := gorm.Open("postgres", "user=postgres password=password dbname=testgorm sslmode=disable")
+const dsn = "user=postgres password=password dbname=testgorm sslmode=disable"
 
+// User struct defines a data field for a user
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+	fmt.Println("{}", users)
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func newUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "New user succesfully created")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	var user User
+	db.Where("name = ?", name).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "Successfully deleted user")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name, email := vars["name"], vars["email"]
+
+	var user User
+	db.Where("name = ?", name).Find(&user)
+	db.Delete(&user)
+
+	user.Email = email
+
+	fmt.Fprintf(w, "Successfully updated user")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/users", allUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{name}", deleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", updateUser).Methods("PUT")
+	myRouter.HandleFunc("/user/{name}/{email}", newUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func initialMigration() {
+	db, err := gorm.Open("postgres", dsn)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	database := db.DB()
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+}
 
-	err = database.Ping()
+func main() {
+	fmt.Println("Go ORM Tutorial")
 
-	if err != nil {
-		panic(err.Error())
-	}
+	// Add the call to our new initialMigration function
+	initialMigration()
 
-	fmt.Println("Connection to postgresql successfully done!")
+	// Handle Subsequent requests
+	handleRequests()
 }
