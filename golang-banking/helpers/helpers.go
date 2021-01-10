e925d2e0ef5f7fbc56ef873ba54d8b2571eff1ab
Muhammad Hasan <muhammadhasan50@gmail.com>
2021-01-10 16:50:56 +0700

    ok

--- golang-banking/helpers/helpers.go
+++ golang-banking/helpers/helpers.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"golang.org/x/crypto/bcrypt"
-	"hasan.com/go-bank-backend/interfaces"
 )
 
 // HandleErr is a function to handle error, it will panic an error message
